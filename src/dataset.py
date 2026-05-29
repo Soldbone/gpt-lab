44deee94fc4f03d8dca941ed635d8bfa345bc27c
Easyseob <83761245+Soldbone@users.noreply.github.com>
2026-05-29 22:08:42 +0900

    feat(GPTDataset): add __init__() and __len__()
    - init self.input_ids and self.target_ids for training

--- src/dataset.py
+++ src/dataset.py
@@ -25,16 +25,17 @@ class GPTDataset(Dataset):
         self.token_ids = token_ids
         self.context_length = context_length
         self.stride = stride if stride is not None else context_length
-        # TODO: 만들 수 있는 학습 샘플 개수를 self._length에 저장하세요.
-        
+
         self.input_ids = []
         self.target_ids = []
         
         for i in range(0, len(token_ids) - context_length, self.stride):
             input_chunk = token_ids[i:i + context_length]
-            target_chunk = token_ids[i + 1: i + context_length + 1]
+            target_chunk = token_ids[i + 1 : i + context_length + 1]
             self.input_ids.append(torch.tensor(input_chunk))
             self.target_ids.append(torch.tensor(target_chunk))
+
+        # TODO: 만들 수 있는 학습 샘플 개수를 self._length에 저장하세요.
         self._length = len(self.input_ids)
         # raise NotImplementedError("GPTDataset.__init__에서 self._length를 구현하세요.")
 
